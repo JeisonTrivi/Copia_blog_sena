c0c42974aa5ed5c9237c5d219e8f0722c9a3ed70
Jeison Trivino <jeisontrivino0617@gmail.com>
2022-10-27 11:28:39 -0500

    cambios  body

--- Javascript/js.js
+++ Javascript/js.js
@@ -3,5 +3,9 @@ let hora_actual = new Date();
 let hora_parseada = new Date().toLocaleDateString('es', { weekday:"long", year:"numeric", month:"long", day:"numeric"})
 btn_menu.innerHTML = hora_parseada
 
-let hora = document.getElementById("hora");
-hora.innerHTML = hora_parseada
+let hora = document.getElementsByClassName("hora");
+console.table(hora)
+for (let index = 0; index < hora.length; index++) {
+    hora[index].innerHTML = hora_parseada
+    
+}
