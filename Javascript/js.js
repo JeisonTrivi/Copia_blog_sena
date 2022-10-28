073ab42b56781ee1df668bee0cf89f4e2b03d2f2
Jeison Triviño <jeisontrivino0617@gmail.com>
2022-10-28 00:13:54 -0500

    cambios en footer

--- Javascript/js.js
+++ Javascript/js.js
@@ -2,10 +2,11 @@ let btn_menu = document.getElementById("hora_actual");
 let hora_actual = new Date();
 let hora_parseada = new Date().toLocaleDateString('es', { weekday:"long", year:"numeric", month:"long", day:"numeric"})
 btn_menu.innerHTML = hora_parseada
+let solo_hora =hora_actual.toLocaleTimeString()
 
 let hora = document.getElementsByClassName("hora");
-console.table(hora)
+console.table(solo_hora)
 for (let index = 0; index < hora.length; index++) {
-    hora[index].innerHTML = hora_parseada
+    hora[index].innerHTML = solo_hora
     
 }
